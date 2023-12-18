85d1f4cdf1ddf787c5dab601383eefca68ea0464
phuongansone <phuongansone@gmail.com>
2023-12-19 00:37:08 +0700

    Delete hotel-booking directory

--- hotel-booking/src/main/java/com/hotel/mvp/hotelbooking/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.mvp.hotelbooking.repository;
-
-import com.hotel.mvp.hotelbooking.entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
